e2384c94a7dff07da1884ff84035ec7f29b2d78e
mkbertrand <mileskbertrand@gmail.com>
2023-10-23 12:23:04 -0400

    Fixed bug with 1st Vespers tags not being included when no potential primary I Vespers occurred

--- prioritizer.py
+++ prioritizer.py
@@ -53,47 +53,45 @@ def getvespers(day):
         for i in iivespers:
             if i == iivespersprimarycandidates[0]:
                 i.add('primarium')
-    if len(ivespersprimarycandidates) == 0:
-        return iivespers
-    else:
+    if not len(ivespersprimarycandidates) == 0:
         for i in ivespers:
             if i == ivespersprimarycandidates[0]:
                 i.add('primarium')
-        # Final product
-        vesperal = iivespers + ivespers
-        def cycle():
-            for coincidence in coincidencetable:
-                for i in vesperal:
-                    if coincidence['indices'].issubset(i) and i.isdisjoint({'fixum'}):
-                        if not type(coincidence['response']) == list:
-                            perform_action(coincidence, day, i)
-                        else:
-                            for j in coincidence['response']:
-                                for k in vesperal:
-                                    if not k == i and not 'fixum' in k and j['indices'].issubset(k):
-                                        target = i
-                                        if 'target' in j and j['target'] == 'b':
-                                            target = k
-                                        if not (j['response'] == 'commemorandum' and 'commemoratum' in target) and not (j['response'] == 'psalmi' and 'psalmi' in target):
-                                            if j['response'] == 'omittendum':
-                                                vesperal.remove(target)
-                                            elif j['response'] == 'commemorandum':
-                                                if 'primarium' in target:
-                                                    target.remove('primarium')
-                                                target.add('commemoratum')
-                                            elif j['response'] == 'psalmi':
-                                                if 'primarium' in target:
-                                                    target.remove('primarium')
-                                                target.add('psalmi')
-                                            elif j['response'] == 'errora':
-                                                raise RuntimeError(f'Unexpected coincidence between {i} and {k} on day {vesperal}')
-                                            else:
-                                                raise RuntimeError(f'Unexpected response: {j["response"]}')
-                                            return True
-            return False
-        while cycle():
-            pass
-        return vesperal
+    # Final product
+    vesperal = iivespers + ivespers
+    def cycle():
+        for coincidence in coincidencetable:
+            for i in vesperal:
+                if coincidence['indices'].issubset(i) and i.isdisjoint({'fixum'}):
+                    if not type(coincidence['response']) == list:
+                        perform_action(coincidence, day, i)
+                    else:
+                        for j in coincidence['response']:
+                            for k in vesperal:
+                                if not k == i and not 'fixum' in k and j['indices'].issubset(k):
+                                    target = i
+                                    if 'target' in j and j['target'] == 'b':
+                                        target = k
+                                    if not (j['response'] == 'commemorandum' and 'commemoratum' in target) and not (j['response'] == 'psalmi' and 'psalmi' in target):
+                                        if j['response'] == 'omittendum':
+                                            vesperal.remove(target)
+                                        elif j['response'] == 'commemorandum':
+                                            if 'primarium' in target:
+                                                target.remove('primarium')
+                                            target.add('commemoratum')
+                                        elif j['response'] == 'psalmi':
+                                            if 'primarium' in target:
+                                                target.remove('primarium')
+                                            target.add('psalmi')
+                                        elif j['response'] == 'errora':
+                                            raise RuntimeError(f'Unexpected coincidence between {i} and {k} on day {vesperal}')
+                                        else:
+                                            raise RuntimeError(f'Unexpected response: {j["response"]}')
+                                        return True
+        return False
+    while cycle():
+        pass
+    return vesperal
 
 if __name__ == '__main__':
     import argparse
