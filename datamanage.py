mkbertrand/liber-usualis

commit 1df3674b72380e888e8127a6e5b9f218e8c9a309

Added repetita so that there's no intonation mark in the repeats

datamanage.py
@@ -60,6 +60,9 @@ def getbreviarumfiles(queries):
                         icopy['datum'] = icopy['datum'].split('*')[0].rstrip()
                         if not icopy['datum'][-1] in ['.',',','?','!',':',';']:
                             icopy['datum'] += '.'
+                        icopy = copy.deepcopy(i)
+                        icopy['tags'].add('repetita')
+                        icopy['datum'] = icopy['datum'].split('* ')[0] + icopy['datum'].split('* ')[1]
                         added.append(icopy)
                 ret.extend(got)
                 ret.extend(added)

commit 34bf52b0e7a3685fc68d0731a11abe00a98a7ac1

Fixed bug introduced in 1df3674b72380e888e8127a6e5b9f218e8c9a309 that prevented creation of intonata antiphons

datamanage.py
@@ -54,12 +54,13 @@ def getbreviarumfiles(queries):
                         icopy['tags'].add('pars')
                         icopy['datum'] = icopy['datum'].split('*')[1].lstrip()
                         added.append(icopy)
-                    if 'antiphona' in i['tags']:
+                    elif 'antiphona' in i['tags']:
                         icopy = copy.deepcopy(i)
                         icopy['tags'].add('intonata')
                         icopy['datum'] = icopy['datum'].split('*')[0].rstrip()
                         if icopy['datum'][-1] not in ['.',',','?','!',':',';']:
                             icopy['datum'] += '.'
+                        added.append(icopy)
                         icopy = copy.deepcopy(i)
                         icopy['tags'].add('repetita')
                         icopy['datum'] = icopy['datum'].split('* ')[0] + icopy['datum'].split('* ')[1]