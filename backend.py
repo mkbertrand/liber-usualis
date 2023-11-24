b477656ffd1636f4c6cb7b25aafef477d12735b4
mkbertrand <mileskbertrand@gmail.com>
2023-11-24 01:40:19 -0500

    Added force reload

--- backend.py
+++ backend.py
@@ -2,10 +2,16 @@ from bottle import route, run, template
 
 import breviarum
 from datetime import date
-
+from datamanage import getyear, getbreviarumfile
 @route('/breviarum/<hour>')
 def index(hour):
     return breviarum.dump_data(breviarum.hour(hour, date.today()))
+
+@route('/forcereload')
+def forcereload():
+    getyear.cache_clear()
+    getbreviarumfile.cache_clear()
+    return 'Successfully dumped data caches.'
 
 run(host='localhost',port=8080)
 
