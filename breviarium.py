mkbertrand/liber-usualis

commit 2760f3af05ae47b424ffabf10e2126f399593bd1

Fixed manual selection not removing commemoratio tag, Suffrages removed from Advent, ensured feria-major not omitted in Vespers and Compline, removed useless Vigil Vesperal coincidences since Vigils don't even have a Vespers

breviarium.py
@@ -223,6 +223,7 @@ def hour(hour: str, day, forcedprimary=None):
         for i in daytags:
             if forcedprimary.issubset(i):
                 i.add('primarium') 
+                i.remove('commemoratio')
     primary = getbytags(daytags, 'primarium')
     if primary is None and forcedprimary:
         raise RuntimeError('Provided tag(s) not found') 

commit 8cc6c60f717e10176bfaa7eca86f7d1a01f19ecb

Fixed bug introduced in 2760f3a that crashes the program when a day that is manually selected doesn't have the commemoration tag

breviarium.py
@@ -223,7 +223,7 @@ def hour(hour: str, day, forcedprimary=None):
         for i in daytags:
             if forcedprimary.issubset(i):
                 i.add('primarium') 
-                i.remove('commemoratio')
+                i -= {'commemoratio'}
     primary = getbytags(daytags, 'primarium')
     if primary is None and forcedprimary:
         raise RuntimeError('Provided tag(s) not found') 