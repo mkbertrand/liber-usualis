7fdff7a8ab7a75188d81f24c00a1c2462725681a
Albert Emanuel Milani <electrodude512@gmail.com>
2024-04-12 01:04:47 -0400

    frontend/process.py: Clarify some logic, add typing

--- frontend/process.py
+++ frontend/process.py
@@ -4,39 +4,41 @@ import re
 
 textlineclass = 'text-line'
 
-def getchant(src, tags):
+def getchant(src, tags) -> str:
     if 'http' in src:
-        return f'<gabc-chant src="/chant/{urllib.parse.quote(src).replace('/', '%2F')}/{'.'.join(tags)}"></gabc-chant>'
+        return f'<gabc-chant src="/chant/{urllib.parse.quote(src).replace("/", "%2F")}/{".".join(tags)}"></gabc-chant>'
     else:
-        return f'<gabc-chant src="/chant/{src}/{'.'.join(tags)}"></gabc-chant>'
+        return f'<gabc-chant src="/chant/{src}/{".".join(tags)}"></gabc-chant>'
 
-def antiphon(antiphon, neumes):
-    ret = f'<div class="{textlineclass}">'
-    if not neumes:
-        return ret + antiphon['datum'] + '</div>'
-    elif not 'src' in antiphon:
-        return ret + antiphon['datum'] + '</div>'
+def antiphon(antiphon: dict, neumes: bool) -> str:
+    match antiphon:
+        case {"src": src, "tags": tags} if neumes:
+            content = getchant(src, tags)
+        case {"datum": datum}:
+            content = datum
+        case _:
+            raise ValueError(f"Unhandled case: {antiphon!r}")
+
+    return f'<div class="{textlineclass}">{content}</div>'
+
+def responsoriumbreve(data: dict, neumes: bool) -> str:
+    datum = data['datum']
+    if neumes and 'src' in datum[0]:
+        return getchant(datum[0]['src'], data['tags'])
     else:
-        return ret + getchant(antiphon['src'], antiphon['tags']) + '</div>'
-
-def responsoriumbreve(data, neumes):
-    if not neumes or not 'src' in data['datum'][0]:
-        incipit = data['datum'][0]['datum'] if 'datum' in data['datum'][0] else 'Absens'
-        response = data['datum'][1]['datum'] if 'datum' in data['datum'][1] else 'Absens'
-        verse = data['datum'][4]['datum'] if 'datum' in data['datum'][2] else 'Absens'
-        gloria = data['datum'][6] if len(data['datum']) == 9 else 'Absens'
+        incipit = datum[0].get('datum', 'Absens')
+        response = datum[1].get('datum', 'Absens')
+        verse = datum[4].get('datum', 'Absens')
+        gloria = datum[6] if len(datum) == 9 else 'Absens'
         return template('frontend/elements/responsorium-breve.tpl', incipit=incipit, response=response, verse=verse, gloria=gloria)
-    else:
-        return getchant(data['datum'][0]['src'], data['tags'])
 
-def stringhandle(line):
+def stringhandle(line: str) -> str:
     line = line.replace('/', '<br>').replace('N.','<span class=red>N.</span>').replace('V. ', '<span class=red>&#8483;.</span> ').replace('R. br. ', '<span class=red>&#8479;. br. </span> ').replace('R. ', '<span class=red>&#8479;.</span> ').replace('✠', '<span class=red>✠</span>').replace('✙', '<span class=red>✙</span>').replace('+', '<span class=red>†</span>').replace('*', '<span class=red>*</span>')
     return f'<p class={textlineclass}>{line}</p>'
 
-def chomp(gabc, tags):
-
-    mode = None
-    if ('mode:' in gabc):
+def chomp(gabc: str, tags) -> str:
+    mode: str | None = None
+    if 'mode:' in gabc:
         mode = gabc[gabc.index('mode:') + 5:gabc.index('\n', gabc.index('mode:'))].strip()
         if mode.endswith(';'):
             mode = mode[:-1]
@@ -45,9 +47,9 @@ def chomp(gabc, tags):
     if mode:
         gabc = f'mode:{mode};\n{gabc}'
     if 'intonata' in tags:
-        gabc = gabc[:gabc.index('*')] + '(::)'
+        return gabc[:gabc.index('*')] + '(::)'
     elif 'repetita' in tags:
-        gabc = 'initial-style:0;\n' + gabc.replace('*','')[gabc.index('\n') + 1:]
+        return 'initial-style:0;\n' + gabc.replace('*','')[gabc.index('\n') + 1:]
     elif 'responsorium-breve' in tags:
         clef = gabc[:gabc.index(')') + 1]
         incipit = gabc[gabc.index(')') + 1 : gabc.index('*')].strip()
@@ -56,22 +58,22 @@ def chomp(gabc, tags):
         verse = gabc[verseloc : gabc.index('(::)', verseloc)].strip()
         gloria = gabc[gabc.index('(::)',  verseloc) + 4 : -4].strip()
 
-        gabc = f'{clef} {incipit} *(;) {response} (::) <v>\\Rbar</v> {incipit} (;) {response} (::) <v>\\Vbar</v> {verse} *(;) {response} (::) <v>\\Vbar</v> {gloria} (::) <v>\\Rbar</v> {incipit} (;) {response} (::)'
-    return gabc
+        return f'{clef} {incipit} *(;) {response} (::) <v>\\Rbar</v> {incipit} (;) {response} (::) <v>\\Vbar</v> {verse} *(;) {response} (::) <v>\\Vbar</v> {gloria} (::) <v>\\Rbar</v> {incipit} (;) {response} (::)'
+    else:
+        return gabc
 
-def process(data, neumes):
+def process(data, neumes: bool) -> str:
     match data:
-        case dict():
-            if {'formula','responsorium-breve'}.issubset(set(data['tags'])):
-                return responsoriumbreve(data, neumes)
-            elif 'antiphona' in set(data['tags']):
-                return antiphon(data, neumes)
-            elif 'src' in data and neumes:
-                return getchant(data['src'], data['tags'])
-            else:
-                return process(data['datum'], neumes)
+        case {"tags": tags} if {'formula','responsorium-breve'}.issubset(set(tags)):
+            return responsoriumbreve(data, neumes)
+        case {"tags": tags} if 'antiphona' in set(tags):
+            return antiphon(data, neumes)
+        case {"src": src, "tags": tags} if neumes:
+            return getchant(src, tags)
+        case {"datum": datum}:
+            return process(datum, neumes)
         case list():
-            return ''.join([process(v, neumes) for v in data])
+            return ''.join(process(v, neumes) for v in data)
         case str():
             return stringhandle(data)
-    raise Exception('Illegal data type')
+    raise ValueError(f"Unhandled case: {data!r}")
