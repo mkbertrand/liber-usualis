d400f26d0628e0374306614363032ed39750611e
mkbertrand <mileskbertrand@gmail.com>
2024-06-27 23:40:57 -0400

    renderer works better with translations

--- frontend/renderer.py
+++ frontend/renderer.py
@@ -4,23 +4,6 @@ import re
 import requests
 import json
 
-def getchant(src, tags) -> str:
-    if 'http' in src:
-        return f'<gabc-chant src="/chant/{urllib.parse.quote(src).replace("/", "%2F")}/{".".join(tags)}"></gabc-chant>'
-    else:
-        return f'<gabc-chant src="/chant/{src}/{".".join(tags)}"></gabc-chant>'
-
-def antiphon(antiphon: dict, neumes: bool) -> str:
-    match antiphon:
-        case {"src": src, "tags": tags} if neumes:
-            content = getchant(src, tags)
-        case {"datum": datum}:
-            content = datum
-        case _:
-            raise ValueError(f"Unhandled case: {antiphon!r}")
-
-    return f'<div class="text-line">{content}</div>'
-
 def responsoriumbreve(data: dict, neumes: bool) -> str:
     datum = data['datum']
     if neumes and 'src' in datum[0]:
@@ -29,6 +12,8 @@ def responsoriumbreve(data: dict, neumes: bool) -> str:
         return template('frontend/components/responsorium-breve.tpl', incipit=datum[0].get('datum', 'Absens'), response=datum[1].get('datum', 'Absens'), verse=datum[4].get('datum', 'Absens'), gloria=datum[6] if len(datum) == 9 else 'Absens')
 
 def stringhandle(line: str) -> str:
+    if type(line) is list:
+        return '<br>'.join([stringhandle(i) for i in line])
     line = line.replace('/', '<br>').replace('\n', '<br>')
     # Has to be reversed since if there exist verse numbers like 10 and 1, the 1 will be replaced in both, and then the 0 will be ignored, and not included as a verse number.
     for i in reversed(re.findall('[0-9]+', line)):
@@ -41,26 +26,34 @@ def render(data, parameters, language = None, translation = None) -> str:
         if not tran is None:
             translation = tran['datum']
     match data:
-        case {"tags": tags} if {'formula','responsorium-breve'}.issubset(set(tags)):
+        case {'tags': tags} if {'formula','responsorium-breve'}.issubset(set(tags)):
             return responsoriumbreve(data, parameters['chant'])
-        case {"tags": tags} if 'antiphona' in set(tags):
-            return antiphon(data, parameters['chant'])
-        case {"src": src, "tags": tags} if parameters['chant']:
-            return getchant(src, tags)
+        case {'src': src, 'tags': tags} if parameters['chant']:
+            if translation:
+                return template('frontend/components/chant.tpl', src=src, tags='.'.join(tags), translation=stringhandle(translation))
+            else:
+                return template('frontend/components/chant.tpl', src=src, tags='.'.join(tags))
         case {'tags': tags, 'datum': datum} if any('psalmi' in i for i in tags):
             return render(datum, parameters, language, translation)
-        case {"datum": datum}:
+        case {'datum': datum}:
             return render(datum, parameters, language, translation)
         case list():
-            if translation and len(translation) == len(data):
+            if translation and len(translation) <= len(data):
+                if len(translation) < len(data):
+                    translation.extend([""] * (len(data) - len(translation)))
                 ret = ''
-                for i in range(0, len(data)):
-                    ret += render(data[i], parameters, language, translation[i])
+                for (i, j) in zip(data, translation):
+                    ret += render(i, parameters, language, j)
                 return ret
             return ''.join(render(v, parameters, language, None) for v in data)
         case str():
             if translation:
-                return template('frontend/components/text.tpl', content=stringhandle(data), translation=translation)
+                if len(data.split('\n')) == len(translation.split('\n')):
+                    ret = ''
+                    for (i, j) in zip(data.split('\n'), translation.split('\n')):
+                        ret += template('frontend/components/text.tpl', content=stringhandle(i), translation=stringhandle(j))
+                    return ret
+                return template('frontend/components/text.tpl', content=stringhandle(data), translation=stringhandle(translation))
             else:
                 return template('frontend/components/text.tpl', content=stringhandle(data))
     raise ValueError(f"Unhandled case: {data!r}")
