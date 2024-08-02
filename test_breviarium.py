a6fca6a3566d57240fccf4fed0e955de4054cde0
mkbertrand <mileskbertrand@gmail.com>
2024-08-02 17:34:56 -0400

    bug hunting in breviarium module

--- test_breviarium.py
+++ test_breviarium.py
@@ -1,12 +1,14 @@
 import pytest
 from datetime import date, timedelta
 import random
+import warnings
 
 import breviarium
 
 @pytest.mark.parametrize('dateoffset', random.sample(range(0,365), k=20))
 def test_breaks(dateoffset: int) -> None:
     day = date(2000, 1, 1) + timedelta(days=dateoffset)
+    warnings.filterwarnings('ignore')
     for hour in ['matutinum', 'laudes', 'prima', 'tertia', 'sexta', 'nona', 'vesperae', 'completorium']:
             breviarium.hour('breviarium-1888', hour, day)
 
