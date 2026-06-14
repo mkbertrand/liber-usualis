535a35c187c1530d938e024542910a68a0921252
mkbertrand <mileskbertrand@gmail.com>
2026-06-14 05:23:15 -0500

    Fixed bug causing wrong suggestSelectedOccasion usage in private recitation

--- web/resources/js/ambit.js
+++ web/resources/js/ambit.js
@@ -89,10 +89,6 @@ class Ambit {
 		return this.occasions[(this.idindex(current) + 1) % this.occasions.length];
 	}
 
-	canIncrementTo(datedOccasion) {
-		return true;
-	}
-
 	// Based on the hour of the day, suggest which occasion should be presented if no external information is available - e.g. if a user loads the page for the first time ever at 9AM, suggest he pray Terce. This is intended to be overwritten manually by individual objects in the Ambit class.
 	suggestSelectedOccasion(hour) {
 		return this.occasions[0];
@@ -312,8 +308,10 @@ function defineambit(desired, choral = true) {
 	if (choral) {
 		return ambit;
 	} else {
-		return new Ambit(ambit.occasions.map(
+		ret = new Ambit(ambit.occasions.map(
 			(occasion) => new Occasion(occasion.name, (occasion.id == 'matutinum' ? occasion.rites : occasion.rites.filter((rite) => rite.what != 'antiphona-bmv')), occasion.id)
 		));
+		ret.suggestSelectedOccasion = ambit.suggestSelectedOccasion;
+		return ret;
 	}
 }
