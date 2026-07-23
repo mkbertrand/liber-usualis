8e08f7e95b178387c4479bb9e65866d226310149
mkbertrand <mileskbertrand@gmail.com>
2026-07-23 10:13:17 -0500

    Some restructuring for formatPsalm

--- web/resources/pray/js/psalmify.js
+++ web/resources/pray/js/psalmify.js
@@ -1,24 +1,31 @@
-function formatPsalm(psalm) {
-  headers = psalm.match(/\[.+?\]\n/g);
-  for (let oldHeader of headers) {
-    newHeader = oldHeader.slice(1, -2).replace(':', '. ') + '.';
-    numeral = newHeader.match(/\s([IVXLC]+)[\s|\.]/);
-    if (numeral) {
-      numeral = numeral[1];
-      vals = {'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
-      number = 0;
-      for (var j = 0; j < numeral.length; j++) {
-        if (j != numeral.length - 1 && vals[numeral[j]] < vals[numeral[j + 1]]) {
-          number += vals[numeral[j + 1]] - vals[numeral[j]];
-          j++;
-        } else {
-          number += vals[numeral[j]];
-        }
+function formatHeader(header) {
+  header = header.slice(1, -2).replace(':', '. ') + '.';
+  numeral = header.match(/\s([IVXLC]+)[\s|\.]/);
+  if (numeral) {
+    numeral = numeral[1];
+    vals = {'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
+    number = 0;
+    for (var j = 0; j < numeral.length; j++) {
+      if (j != numeral.length - 1 && vals[numeral[j]] < vals[numeral[j + 1]]) {
+        number += vals[numeral[j + 1]] - vals[numeral[j]];
+        j++;
+      } else {
+        number += vals[numeral[j]];
       }
-      newHeader = newHeader.replace(numeral, number);
     }
-    psalm = psalm.replace(oldHeader, '[' + newHeader + ']\n');
   }
-  console.log(psalm);
-  return psalm;
+  return `[${header.replace(numeral, number)}]\n`;
+}
+
+function formatPsalm(psalm) {
+  ret = '';
+  for (p of psalm.split(/(\[.+?\]\n)/).slice(1)) {
+    console.log(p);
+    if (p.startsWith('[')) {
+      ret += formatHeader(p);
+    } else {
+      ret += p;
+    }
+  }
+  return ret;
 }
