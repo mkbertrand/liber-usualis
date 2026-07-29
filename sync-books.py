864ba8e158ae30b809b04cf92aa9c448314ef356
mkbertrand <mileskbertrand@gmail.com>
2026-07-29 14:57:59 -0500

    Fallback if books.json is missing

--- sync-books.py
+++ sync-books.py
@@ -4,9 +4,12 @@ from pathlib import Path
 
 from datamanage import DATA_ROOT
 
-for book in json.load(open('books.json')):
-    destination = DATA_ROOT.joinpath('generated').joinpath(book['loc'])
-    if destination.exists():
-        subprocess.run(['git', '-C', destination, 'pull'], check=True)
-    else:
-        subprocess.run(['git', 'clone', book['src'], '--branch', book['branch'], destination])
+if Path('books.json').exists():
+    for book in json.load(open('books.json')):
+        destination = DATA_ROOT.joinpath('generated').joinpath(book['loc'])
+        if destination.exists():
+            subprocess.run(['git', '-C', destination, 'pull'], check=True)
+        else:
+            subprocess.run(['git', 'clone', book['src'], '--branch', book['branch'], destination])
+else:
+    print('books.json not detected.')
